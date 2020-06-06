354999e3c33a0c95399a7ad7e44139507f9f834e
JanSlechta360 <60642873+JanSlechta360@users.noreply.github.com>
2020-06-06 15:25:01 +0200

    c
    c

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+      <body>
+<form action="submit.php" method="post">
+           <label for="A">Prvni cislo</label>
+           <input type="number" name="A" id="A">
+ 
+           <label for="B">Druhe cislo</label>
+           <input type="number" name="B" id="B">
+
+           <input type="submit" value="vypocitat">
+
+      </body>
+</html>
